c6e70c71754bc4414573da549a2e3ca3d41eeb4b
AmanGuptaa <aman4.gupta.4@gmail.com>
2016-09-23 19:07:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,17 +55,7 @@ var articles = {
                         </p>`
                 
             }
-            'message':{
-              title:'Ugent message | Aman Gupta ',
-              heading:'Urgent msg',
-              date:'Sep 26, 2016',
-              content:`
-                        <p>
-                            hi akanhsa..!! i am Aman .
-                            I never thought that you wont tell me about  thingssss..!!!
-                        </p>`
-                
-            }
+            
 };
   
         
