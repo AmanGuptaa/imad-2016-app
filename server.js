5b9e1d50bad3873c04e73038f0438cd8b8310b9e
AmanGuptaa <aman4.gupta.4@gmail.com>
2016-09-23 17:56:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,82 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+  title:'Article One | Aman Gupta ',
+  heading:'Article One',
+  date:'Sep 23, 2016',
+  content:`
+          <p>
+             This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+             This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+              This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+              This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+           </p> 
+           <p>
+             This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+             This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+              This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+              This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+           </p>
+           <p>
+             This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+             This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+              This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+              This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+           </p>`
+           
+  
+  
+    
+};
+
+function createTemplate(date){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    
+}
+var htmltemplate=`
+   
+<html>
+  <head>
+    <title>
+       ${title}
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/> 
+    <link href="/ui/style.css" rel="stylesheet" />
+     
+   </head>
+   <body>
+    <div class="container">
+      <div>
+   	   <a href="/">Home</a>
+      </div>
+      <h3>
+        ${heading}
+       </h3>
+       <div>
+         ${date}
+        </div>
+          ${content}
+         </div>
+      
+       </body>
+</html>
+
+`;
+return htmltemplate;
+}
+
+
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){
