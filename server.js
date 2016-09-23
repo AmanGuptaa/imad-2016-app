e9788f832695316155199b5edb7fea3abeb9b0e2
AmanGuptaa <aman4.gupta.4@gmail.com>
2016-09-23 17:13:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,19 +6,19 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-    res.send('Article one requested and served ..!! ')
+    res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 });
 
 app.get('/article-two',function(req,res){
-    res.send('Article two requested and served ..!! ')
+    res.sendFile(path.join(__dirname, 'ui', 'Article-two.html'));
 });
 
 app.get('/article-three',function(req,res){
-    res.send('Article three requested and served ..!! ')
+    res.sendFile(path.join(__dirname, 'ui', 'Article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
